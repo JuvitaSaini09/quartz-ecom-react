53be85ee69c221c7c58b99d7b0716662c74d7a73
JuvitaSaini09 <juvitasaini@gmail.com>
2022-04-06 23:07:18 +0530

    docs : added map for displaying cards

--- src/components/featuredCategories/featuredCategories.js
+++ src/components/featuredCategories/featuredCategories.js
@@ -1,214 +1,104 @@
 import React from 'react'
-import atomicHabits from "../../images/atomic-habits.jpg"
-import fiveAmClub from "../../images/5am-club.jpg"
-import alchemist from "../../images/alchemist.jpg"
-import ikigai from "../../images/ikigai.jpg"
+import axios from "axios"
+import { useEffect, useState } from 'react';
+
 
 
 function FeaturedCategories() {
+  const [data,setData]=useState([]);
+  useEffect(async () => {
+    try {
+      const response = await axios.get("/api/products");
+      setData(response.data.products)
+  }catch(e) {
+      console.error(e);
+  }
+  },[])
+
+  const featuredProducts=[...data].filter(item=>{
+    return  (item.title==="Atomic Habits"||item.title==="The 5 Am Club"||item.title==="The House Of Strangers Stories"||item.title==="The Alchemist")
+  })
+
+  const newArrivalProducts=[...data].filter(item=>{
+    return (item.title==="Unfinished"||item.title==="The Fitness Currency"||item.title==="Agni Ki Udaan"||item.title==="Mahabharata")
+  })
+
   return (
     <>
 <div>
   <section className="feature-categories">
     <div className="feat-heading"> <h2>Featured Categories</h2></div>
     <div className="feature-container">
-      <a href="https://github.com/JuvitaSaini09/">
-        <div className="card-featured">
-          <div className="card6 card">
-            <div className="card-badge">
-              <button className="heart-badge "><span className="heart">♡</span></button>
-              <img className="card-image" alt="card" src={atomicHabits} />
-              <div className="card-text card6-text">
-                <header>
-                  <h3>Atomic Habits</h3><button className="rate-btn"><span>★</span>4.5</button>
-                </header>
-                <div className="card-info">
-                  <p>Atomic Habits</p>
-                  <div className="price">₹. 300
-                    <span className="price-old">₹ 600</span>
-                  </div>
-                </div>
-              </div>
-              <footer>
-                <button className="btn-cart">Add to cart</button>
-              </footer>
-            </div>
-          </div>
-        </div>
-      </a>
-      <a href="https://github.com/JuvitaSaini09/">
-        <div className="card-featured">
-          <div className="card6 card">
-            <div className="card-badge">
-              <button className="heart-badge "><span className="heart">♡</span></button>
-              <img className="card-image" alt="card" src={ikigai} />
-              <div className="card-text card6-text">
-                <header>
-                  <h3>Ikigai</h3><button className="rate-btn"><span>★</span>4.5</button>
-                </header>
-                <div className="card-info">
-                  <p>Ikigai</p>
-                  <div className="price">₹. 300
-                    <span className="price-old">₹. 600</span>
-                  </div>
-                </div>
-              </div>
-              <footer>
-                <button className="btn-cart">Add to cart</button>
-              </footer>
-            </div>
-          </div>
-        </div>
-      </a>
-      <a href="https://github.com/JuvitaSaini09/">
-        <div className="card-featured">
-          <div className="card6 card">
-            <div className="card-badge">
-              <button className="heart-badge "><span className="heart">♡</span></button>
-              <img className="card-image" alt="card" src={alchemist} />
-              <div className="card-text card6-text">
-                <header>
-                  <h3>Alchemist</h3><button className="rate-btn"><span>★</span>4.5</button>
-                </header>
-                <div className="card-info">
-                  <p>Alchemist</p>
-                  <div className="price">₹. 300
-                    <span className="price-old">₹. 600</span>
-                  </div>
-                </div>
-              </div>
-              <footer>
-                <button className="btn-cart">Add to cart</button>
-              </footer>
-            </div>
-          </div>
-        </div>
-      </a>
-      <a href="https://github.com/JuvitaSaini09/">
-        <div className="card-featured">
-          <div className="card6 card">
-            <div className="card-badge">
-              <button className="heart-badge "><span className="heart">♡</span></button>
-              <img className="card-image" alt="card" src={fiveAmClub} />
-              <div className="card-text card6-text">
-                <header>
-                  <h3>Atomic Habits</h3><button className="rate-btn"><span>★</span>4.5</button>
-                </header>
-                <div className="card-info">
-                  <p>Atomic Habits</p>
-                  <div className="price">₹. 300
-                    <span className="price-old">₹. 600</span>
-                  </div>
-                </div>
-              </div>
-              <footer>
-                <button className="btn-cart">Add to cart</button>
-              </footer>
-            </div>
-          </div>
-        </div>
-      </a>
+
+{
+ featuredProducts.map(item=>{
+    return(
+     <div className="card-featured">
+   <div className="card6 card">
+     <div className="card-badge">
+       <button className="heart-badge "><span ><i className="fas fa-heart fa-2x"></i></span></button>
+       <a href="/">
+         <img className="card-image" alt="card" src={item.src} />
+         <div className="card-text card6-text">
+           <header>
+             <h3>{item.title}</h3><button className="rate-btn"><span>★</span>{item.rating}</button>
+           </header>
+           <div className="card-info">
+             <p>{item.categoryName},{item.author}</p>
+             <div className="price">₹. {item.price}
+               <span className="price-old">₹ {item.oldPrice}</span>
+             </div>
+           </div>
+         </div>
+       </a>
+       <footer>
+         <button className="btn-cart" >Add to cart</button>
+       </footer>
+     </div>
+   </div>
+ </div>
+    )
+  })
+}
+
+   
+      
     </div>
   </section>
   <section className="feature-categories">
     <div className="feat-heading"> <h2>New arrivals</h2></div>
     <div className="feature-container">
-      <a href="https://github.com/JuvitaSaini09/">
-        <div className="card-featured">
-          <div className="card6 card">
-            <div className="card-badge">
-              <button className="heart-badge "><span className="heart">♡</span></button>
-              <img className="card-image" alt="card" src={atomicHabits} />
-              <div className="card-text card6-text">
-                <header>
-                  <h3>Atomic Habits</h3><button className="rate-btn"><span>★</span>4.5</button>
-                </header>
-                <div className="card-info">
-                  <p>Atomic habits</p>
-                  <div className="price">₹. 300
-                    <span className="price-old">₹. 600</span>
-                  </div>
-                </div>
-              </div>
-              <footer>
-                <button className="btn-cart">Add to cart</button>
-              </footer>
-            </div>
-          </div>
-        </div>
-      </a>
-      <a href="https://github.com/JuvitaSaini09/">
-        <div className="card-featured">
-          <div className="card6 card">
-            <div className="card-badge">
-              <button className="heart-badge "><span className="heart">♡</span></button>
-              <img className="card-image" alt="card" src={ikigai} />
-              <div className="card-text card6-text">
-                <header>
-                  <h3>Ikigai</h3><button className="rate-btn"><span>★</span>4.5</button>
-                </header>
-                <div className="card-info">
-                  <p>Ikigai</p>
-                  <div className="price">₹. 300
-                    <span className="price-old">₹. 600</span>
-                  </div>
-                </div>
-              </div>
-              <footer>
-                <button className="btn-cart">Add to cart</button>
-              </footer>
-            </div>
-          </div>
-        </div>
-      </a>
-      <a href="https://github.com/JuvitaSaini09/">
-        <div className="card-featured">
-          <div className="card6 card">
-            <div className="card-badge">
-              <button className="heart-badge "><span className="heart">♡</span></button>
-              <img className="card-image" alt="carde" src={alchemist} />
-              <div className="card-text card6-text">
-                <header>
-                  <h3>Alchemist</h3><button className="rate-btn"><span>★</span>4.5</button>
-                </header>
-                <div className="card-info">
-                  <p>Alchemist</p>
-                  <div className="price">₹. 300
-                    <span className="price-old">₹. 600</span>
-                  </div>
-                </div>
-              </div>
-              <footer>
-                <button className="btn-cart">Add to cart</button>
-              </footer>
-            </div>
-          </div>
-        </div>
-      </a>
-      <a href="https://github.com/JuvitaSaini09/">
-        <div className="card-featured">
-          <div className="card6 card">
-            <div className="card-badge">
-              <button className="heart-badge "><span className="heart">♡</span></button>
-              <img className="card-image" alt="card" src={fiveAmClub} />
-              <div className="card-text card6-text">
-                <header>
-                  <h3>5AM Club</h3><button className="rate-btn"><span>★</span>4.5</button>
-                </header>
-                <div className="card-info">
-                  <p>5AM club</p>
-                  <div className="price">₹. 300
-                    <span className="price-old">₹. 600</span>
-                  </div>
-                </div>
-              </div>
-              <footer>
-                <button className="btn-cart">Add to cart</button>
-              </footer>
-            </div>
-          </div>
-        </div>
-      </a>
+    {
+newArrivalProducts.map(item=>{
+    return(
+     <div className="card-featured">
+   <div className="card6 card">
+     <div className="card-badge">
+       <button className="heart-badge "><span ><i className="fas fa-heart fa-2x"></i></span></button>
+       <a href="/">
+         <img className="card-image" alt="card" src={item.src} />
+         <div className="card-text card6-text">
+           <header>
+             <h3>{item.title}</h3><button className="rate-btn"><span>★</span>{item.rating}</button>
+           </header>
+           <div className="card-info">
+             <p>{item.categoryName},{item.author}</p>
+             <div className="price">₹. {item.price}
+               <span className="price-old">₹ {item.oldPrice}</span>
+             </div>
+           </div>
+         </div>
+       </a>
+       <footer>
+         <button className="btn-cart" >Add to cart</button>
+       </footer>
+     </div>
+   </div>
+ </div>
+    )
+  })
+}
+
     </div>
   </section>
 </div>
