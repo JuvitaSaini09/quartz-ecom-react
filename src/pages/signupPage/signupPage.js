597e9f52fcc550f3868dde08526c05a560fd7575
JuvitaSaini09 <juvitasaini@gmail.com>
2022-04-07 23:48:27 +0530

    feat:add authentication page

---
+++ src/pages/signupPage/signupPage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Footer, Navbar, Signup } from '../../components/allComponents'
+import "./signupPage.css"
+
+function SingupPage() {
+  return (
+    <>
+    <Navbar />
+    <Signup />
+    <Footer />
+    </>
+  )
+}
+
+export { SingupPage }
