f8ae647c9c5309162868f084f5ff9d89fa7fd896
JuvitaSaini09 <juvitasaini@gmail.com>
2023-02-17 13:39:25 +0530

    feat: added useState

--- src/backend/db/users.js
+++ src/backend/db/users.js
@@ -17,3 +17,5 @@ export const users = [
     updatedAt: formatDate(),
   },
 ];
+
+
--- src/components/authentication/login/login.js
+++ src/components/authentication/login/login.js
@@ -1,7 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 function Login() {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  
+  const loginHandler=()=>{
+   if(email!==''  && password !== '')
+   console.log(email,password)
+   else console.log("Error : input both fields")
+  }
   return (
     <>
     <main className="login-page-container">
@@ -10,13 +19,13 @@ function Login() {
   </div>
   <div className="text-input-container">
     <label htmlFor="text-input">E-mail</label>
-    <input type="email" className="text-input" placeholder="Your Email" /><span className="login-icon"><i className="fas fa-envelope" /></span>
+    <input type="email" className="text-input" placeholder="Your Email" onChange={e=>setEmail(e.target.value)} value={email}/><span className="login-icon"><i className="fas fa-envelope" /></span>
   </div>
   <div className="text-input-container">
     <label htmlFor="text-input" auto-complete="new-password">Password</label>
-    <input type="password" className="text-input" placeholder="Your password" /><span className="login-icon"><i className="fas fa-eye" /></span>
+    <input type="password" className="text-input" placeholder="Your password" onChange={e=>setPassword(e.target.value)} value={password} /><span className="login-icon"><i className="fas fa-eye" /></span>
   </div>
-  <div className="login-bottom"><button>Login</button>
+  <div className="login-bottom"><button onClick={loginHandler}>Login</button>
     <p>Create Account <Link to="/signupPage" style={{color:"blue"}}>Sign up</Link></p>
   </div>
 </main>
