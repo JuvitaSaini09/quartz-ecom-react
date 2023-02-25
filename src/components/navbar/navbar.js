d690db978797d00d4d369afd9d1cb9de1c676677
JuvitaSaini09 <juvitasaini@gmail.com>
2023-02-25 22:23:06 +0530

    feat : added user name

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 const Navbar = () => {
+
+  const user= JSON.parse(localStorage.getItem('user'))
   return (
     <>
       <header className="header-nav">
@@ -15,20 +17,21 @@ const Navbar = () => {
 
               <Link to="/ProductPage"><li><div  >PRODUCTS</div></li></Link>
 
-              {
-                localStorage.getItem("token") 
-                ? <Link to="/logoutPage" onClick={()=>{
-                  localStorage.clear();
-                  
-                }}><li><div >LOGOUT</div></li></Link>
-                : <Link to="/loginPage"><li><div >ACCOUNT</div></li></Link>
-                 
-              }
+             
 
               <Link to="/wishlistPage" style={{color:"transparent"}}>  <li><div > <i className="fas fa-heart" /></div></li></Link>
 
               <Link to="/cartPage" style={{color:"transparent"}}> <li><div><i className="fas fa-shopping-bag" /></div></li> </Link>
 
+              {
+                localStorage.getItem("token") 
+                ? <Link to="/logoutPage" onClick={()=>{
+                  localStorage.clear();
+                  
+                }}><li><div >{user.firstName}  <i className="fas fa-sign-in-alt"></i></div></li></Link>
+                : <Link to="/loginPage"><li><div >ACCOUNT</div></li></Link>
+                 
+              }
             </ul>
           </nav>
         </div>
