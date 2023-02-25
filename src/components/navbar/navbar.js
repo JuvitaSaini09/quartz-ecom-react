6c601b4f5eecd0c245acdd1dbaa5564117432d9f
JuvitaSaini09 <juvitasaini@gmail.com>
2023-02-25 21:33:17 +0530

    fix : fixed the code

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -13,21 +13,21 @@ const Navbar = () => {
 
               <Link to="/"><li><div>HOME</div></li></Link>
 
-              <Link to="/ProductPage"><li><div href="/" >PRODUCTS</div></li></Link>
+              <Link to="/ProductPage"><li><div  >PRODUCTS</div></li></Link>
 
               {
                 localStorage.getItem("token") 
                 ? <Link to="/logoutPage" onClick={()=>{
                   localStorage.clear();
                   
-                }}><li><div href="/components/product-page/login-page.html">LOGOUT</div></li></Link>
-                : <Link to="/loginPage"><li><div href="/">ACCOUNT</div></li></Link>
+                }}><li><div >LOGOUT</div></li></Link>
+                : <Link to="/loginPage"><li><div >ACCOUNT</div></li></Link>
                  
               }
 
-              <Link to="/wishlistPage">  <li><div href="/"> <i className="fas fa-heart" /></div></li></Link>
+              <Link to="/wishlistPage" style={{color:"transparent"}}>  <li><div > <i className="fas fa-heart" /></div></li></Link>
 
-              <Link to="/cartPage"> <li><div><i className="fas fa-shopping-bag" /></div></li> </Link>
+              <Link to="/cartPage" style={{color:"transparent"}}> <li><div><i className="fas fa-shopping-bag" /></div></li> </Link>
 
             </ul>
           </nav>
